c8b1e4ff264e64f789fab4850bb44b04ae6caf06
Vytenis Gelažius <vytenis.gelazius@telesoftas.com>
2023-04-10 07:47:18 +0300

    COL-32: Update author mutation

---
+++ api/src/Mutation/UpdateAuthorMutation.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mutation;
+
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use App\Service\MutationResponseFactory;
+use Overblog\GraphQLBundle\Definition\Argument;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+use Overblog\GraphQLBundle\Validator\Exception\ArgumentsValidationException;
+use Overblog\GraphQLBundle\Validator\InputValidator;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+final readonly class UpdateAuthorMutation implements MutationInterface, AliasedInterface
+{
+    public function __construct(
+        private AuthorRepository $authorRepository,
+        private MutationResponseFactory $mutationResponseFactory
+    ) {
+    }
+
+    /**
+     * @throws ArgumentsValidationException
+     */
+    public function __invoke(Argument $arguments, InputValidator $validator): array
+    {
+        $author = $this->authorRepository->find(id: $arguments['id']);
+
+        if ($author === null) {
+            return $this->mutationResponseFactory
+                ->failure()
+                ->getResponse();
+        }
+
+        $violations = $validator->validate(throw: false);
+        assert($violations instanceof ConstraintViolationList);
+
+        if ($violations->count()) {
+            return $this->mutationResponseFactory
+                ->violations($violations)
+                ->getResponse();
+        }
+
+        $author = $this->updateAuthor($author, $arguments);
+        $this->authorRepository->save(entity: $author, flush: true);
+
+        return $this->mutationResponseFactory
+            ->success()
+            ->getResponse();
+    }
+
+    public static function getAliases(): array
+    {
+        return [
+            'resolve' => 'UpdateAuthor',
+        ];
+    }
+
+    private function updateAuthor(Author $author, Argument $arguments): Author
+    {
+        $input = $arguments->offsetGet('author');
+        assert(is_array($input));
+
+        return $author->setName($input['name']);
+    }
+}
