86fb148224edd440ad9bb250d88aa0df541e2bba
Vytenis Gelažius <vytenis.gelazius@telesoftas.com>
2023-04-10 17:52:52 +0300

    COL-44: Get users query

---
+++ api/src/Resolver/User/GetCollection.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Resolver\User;
+
+use App\Repository\CategoryRepository;
+use App\Repository\UserRepository;
+use App\Service\CollectionArgumentProvider;
+use Overblog\GraphQLBundle\Definition\Argument;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\QueryInterface;
+
+final readonly class GetCollection implements QueryInterface, AliasedInterface
+{
+    public function __construct(
+        private CollectionArgumentProvider $collectionArgumentProvider,
+        private UserRepository $userRepository,
+    ) {
+    }
+
+    public function __invoke(Argument $arguments): array
+    {
+        [$limit, $orderBy, $criteria] = $this->collectionArgumentProvider->provide($arguments);
+
+        $users = $this->userRepository->findBy(
+            criteria: [],
+            orderBy: [
+                $orderBy => $criteria,
+            ],
+            limit: $limit,
+            offset: 0
+        );
+
+        return [
+            'users' => $users,
+        ];
+    }
+
+    public static function getAliases(): array
+    {
+        return [
+            '__invoke' => 'getUsers',
+        ];
+    }
+}
