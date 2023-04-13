3e1bd4c2cec81bb3f617a7095858c4f998da5484
Vytenis Gelažius <vytenis.gelazius@telesoftas.com>
2023-04-13 06:43:29 +0300

    COL-38: Get books query

--- api/src/Resolver/BookResolver.php
+++ api/src/Resolver/BookResolver.php
@@ -6,6 +6,7 @@ namespace App\Resolver;
 
 use App\Entity\Book;
 use App\Repository\BookRepository;
+use App\Service\CollectionArgumentProvider;
 use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
 use Overblog\GraphQLBundle\Definition\Resolver\QueryInterface;
@@ -14,6 +15,7 @@ final readonly class BookResolver implements QueryInterface, AliasedInterface
 {
     public function __construct(
         private BookRepository $bookRepository,
+        private CollectionArgumentProvider $collectionArgumentProvider,
     ) {
     }
 
@@ -24,8 +26,19 @@ final readonly class BookResolver implements QueryInterface, AliasedInterface
 
     public function getCollection(Argument $arguments): array
     {
+        [$limit, $orderBy, $criteria] = $this->collectionArgumentProvider->provide($arguments);
+
+        $books = $this->bookRepository->findBy(
+            criteria: [],
+            orderBy: [
+                $orderBy => $criteria,
+            ],
+            limit: $limit,
+            offset: 0
+        );
+
         return [
-            'books' => [],
+            'books' => $books,
         ];
     }
 
